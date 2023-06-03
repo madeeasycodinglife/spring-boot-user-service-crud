a65972a52d723ca892f684ccf13276d731bd08d5
Pabitra Bera <126568738+madeeasycodinglife@users.noreply.github.com>
2023-06-03 12:53:57 +0530

    Add files via upload

---
+++ spring-boot-user-service/src/main/java/com/madeeasy/SpringBootUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.madeeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUserServiceApplication.class, args);
+	}
+
+}
---
+++ spring-boot-user-service/src/main/java/com/madeeasy/service/impl/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.madeeasy.service.impl;
+
+import com.madeeasy.entity.User;
+import com.madeeasy.repository.UserRepository;
+import com.madeeasy.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(Long id, User user) {
+        user.setId(id);
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
+
